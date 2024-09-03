5bd59ef057844ca5388b9e956844392bd75d4b5b
Samit Koyom <samitkoyom@gmail.com>
2024-09-03 22:22:20 +0700

    update swagger file

--- cmd/api/docs/docs.go
+++ cmd/api/docs/docs.go
@@ -522,7 +522,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:7070",
+    // Host:             "localhost:8080", # localhost
+	// Host:             "localhost:7070", # docker localhost
+	Host:             "go-sample-api-deployed.onrender.com", // Render
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Movies API with GO and PostgreSQL",
